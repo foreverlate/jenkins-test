15a556323b9790c92127c37477e340b81e6df565
foreverlate <92173313+foreverlate@users.noreply.github.com>
2022-03-31 17:03:39 +0800

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,9 +1,18 @@
-def systemout = new StringBuffer(), systemerr = new StringBuffer()
-
-def proc ='./run.sh'.execute()
-
 def runProc(){
-    proc.consumeProcessOutput(systemout, systemerr)
-    proc.waitForProcessOutput()
-    println systemout
+ sh '''
+ #!/bin/bash
+
+value1=10
+echo "value 1 " + $value1
+
+if [[ $value1 -gt 10 ]]
+then
+     echo "The variable is greater than 10."
+elif [[ $value1 -eq 10 ]]
+then
+     echo "The variable is equal to 10."
+else
+     echo "The variable is lesser than 10."
+fi
+'''
 }
